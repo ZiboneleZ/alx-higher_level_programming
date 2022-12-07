3ddeda8a04c7431a5f98000439cd4725f0f14636
ZiboneleZ <zibonelez47@gmail.com>
2022-12-07 13:12:33 -0800

    Fixing the incomplete tasks

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,10 +1,7 @@
-#!/bin/python3
-from variable_load_5 import a
-
-
-def main():
-    print(a)
-
+#!/usr/bin/python3
 
 if __name__ == "__main__":
-    main()
+    
+    import variable_load_5
+    
+    print(variable_load_5.a)
