cae1c0fe19f16cbabcae474aa902e30609648fd6
ZiboneleZ <zibonelez47@gmail.com>
2023-01-11 20:13:43 -0800

    Fixing for next bit

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 import sys
 
-def print_status():    
+
+def print_status():
     '''
         Printing the status of the request
     '''
 
-    counter = 0    
+    counter = 0
     size = 0
     file_size = 0
-    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,   
+    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
                     "403": 0, "404": 0, "405": 0, "500": 0}
-    
-    for l in sys.stdin:        
-        line = l.split()
+
+    for p in sys.stdin:
+        line = p.split()
         try:
             size += int(line[-1])
             code = line[-2]
@@ -28,19 +29,15 @@ def print_status():
             counter = 0
         counter += 1
     if counter < 9:
-        
+
         print("File size: {}".format(size))
-        
+
         for key, val in sorted(status_codes.items()):
-            
+
             if (val != 0):
-                
+
                 print("{}: {}".format(key, val))
                 
-
-                
-
-                
 if __name__ == "__main__":
-    
+
     print_status()
