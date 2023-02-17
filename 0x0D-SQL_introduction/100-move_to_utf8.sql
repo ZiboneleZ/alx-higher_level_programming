127e9e171b16d04f0c6598ba29a05efa46f2c93f
ZiboneleZ <zibonelez47@gmail.com>
2023-02-16 18:36:16 -0800

    Fixing 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,4 @@
--- Converts parts of the database to UTF-8
+-- converts hbtn_0c_0 database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
-ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- doesn't work. ALTER TABLE first_table MODIFY name VARBINARY(255);
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
