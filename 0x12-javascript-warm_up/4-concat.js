f418d889053904531fbba673f3012143fb183901
ZiboneleZ <zibonelez47@gmail.com>
2023-03-14 01:50:22 -0700

    New commit

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-if (process.argv[2]) {
-    console.log(process.argv[2]);
-}
-else {
-    console.log("No argument");
+if (process.argv[2] && process.argv[3] {
+  console.log('${process.argv[2]} is ${process.argv[3]');
+} else {
+  console.log('Two arguments are required');
 }
