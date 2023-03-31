6548a5a3b10882b9777002519f85026024e95372
ZiboneleZ <zibonelez47@gmail.com>
2023-03-30 17:46:45 -0700

    New commit

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-# Send a POST request with a custom header and body to cause the server to respond with "You got me!"
-curl -sX POST -H "Origin: HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
+# Script that makes a request to 0.0.0.0:5000/catch_me and get the response
+curl -sX PUT -L -d "user_id=98" -H "Origin:HolbertonSchool" "0.0.0.0:5000/catch_me"
