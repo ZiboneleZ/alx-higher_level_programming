41f06914951acc4fb2994a8946fecfa9146dcf43
ZiboneleZ <zibonelez47@gmail.com>
2023-03-31 18:02:46 -0700

    Hope it works

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,15 @@
 #!/bin/python3
-"""A script that fetches 
-    https://alx-intranet.hbtn.io/status
+"""A script that fetches
+   https://alx-intranet.hbtn.io/status
 """
 
 if __name__ == '__main__':
     import urllib.request
 
-
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
+    as response:
         webContent = response.read()
         print("Body request:")
         print("\t- type: {}".format(type(webContent)))
         print("\t- content: {}".format(webContent))
         print("\t- utf8 content: {}".format(webContent.decode('utf-8')))
-    
-
