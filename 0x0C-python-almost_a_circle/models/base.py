65199ab578bd7e69ec7ce2fb11e76b143dae5f21
ZiboneleZ <zibonelez47@gmail.com>
2024-02-23 08:01:39 +0000

    Starting the project...

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,12 @@
+class Base:
+    ''' Comment '''
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''Comment'''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
