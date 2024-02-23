751ab12e2814b127431219cb2533d5c5afed5d53
ZiboneleZ <zibonelez47@gmail.com>
2024-02-23 13:50:47 -0800

    Done with number 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -93,6 +93,8 @@ class Rectangle(Base):
         if y is not None:
             self.y = y
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         if args:
             self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
