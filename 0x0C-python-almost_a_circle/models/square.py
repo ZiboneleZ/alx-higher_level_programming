37ecb791b11086095df3de1944a4d819e738604b
ZiboneleZ <zibonelez47@gmail.com>
2024-02-24 23:56:15 -0800

    Done with number 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -20,5 +20,23 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def __update(self, id=None, size=None, x=None, y=None):
+        # This will update the instance att's using below 'update' function
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        # This updates the instance att's using args
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
+
 
 
