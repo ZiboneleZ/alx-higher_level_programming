1baf6e6b277e1990cd5b451a9063945cbe0f81bd
ZiboneleZ <zibonelez47@gmail.com>
2024-02-26 00:44:41 -0800

    Done with the bonus round, phew!

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 from json import dumps, loads
+import csv
 
 class Base:
     ''' Comment '''
@@ -60,3 +61,61 @@ class Base:
         with open(file, "r", encoding="utf-8") as the_file:
             return [cls.create(**another_file) for another_file in cls.from_json_string(the_file.read())]
 
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        # serializes in CSV
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if list_objs is not None:
+            if cls is Rectangle:
+                list_objs = [[obj.id, obj.width, obj.height, obj.x, obj.y]
+                        for obj in list_objs]
+            else:
+                list_objs = [[obj.id, obj.width, obj.height, obj.x, obj.y]
+                        for obj in list_objs]
+        with open('{}.csv'.format(cls.__name__), 'w', newline='', encoding="utf-8") as my_file:
+            written = csv.writer(my_file)
+            written.writerows(list_objs)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        # deserializes in CSV
+        from models.rectangle import Rectangle
+        from models.square import Square
+        listing = []
+        with open('{}.csv'.format(cls.__name__), 'r', newline='', encoding="utf-8") as my_file:
+                reader = csv.reader(my_file)
+                for row in reader:
+                    row = [int(des) for des in row]
+                    if cls is Rectangle:
+                        rect = {"id": row[0], "width": row[1], "height": row[2], "x": row[3], "y": row[4]}
+                    else:
+                        rect = {"id": row[0], "size": row[1], "x": row[2], "y": row[3]}
+                    listing.append(cls.create(**rect))
+        return listing
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        from time import sleep
+        from random import randrange
+        import turtle
+        turtle.Screen().colormode(255)
+        for i in list_rectangles + list_squares:
+            s = turtle.Turtle()
+            s.color((randrange(255), randrange(255), randrange(255)))
+            s.pensize(1)
+            s.penup()
+            s.pendown()
+            s.setpos((i.x + s.pos()[0], i.y - t.pos()[1]))
+            s.pensize(10)
+            s.forward(i.width)
+            s.left(90)
+            s.forward(i.height)
+            s.left(90)
+            s.forward(i.width)
+            s.left(90)
+            s.forward(i.height)
+            s.left(90)
+            s.end_fill()
+
+        sleep(5)
