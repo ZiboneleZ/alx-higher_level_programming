3e6f789430c6b074270c732bf87c87a80b13d3fa
Ubuntu <azureuser@first.nrw32hdpzhnuraqadyrvdmrvrh.bx.internal.cloudapp.net>
2024-05-10 15:15:24 +0000

    New one

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
 """
- script that takes 2 arguments in order to solve this challenge.
+Python script that takes 2 arguments in order to solve this challenge.
 
-The first argument will be the repository name
-The second argument will be the owner name
-You must use the packages requests and sys
-You are not allowed to import packages other than requests and sys
-You don’t need to check arguments passed to the script (number or type)
+Usage: ./100-github_commits.py <repository name> <repository owner>
 """
-
-import sys
+from sys import argv
 import requests
 
-if __name__ == '__main__':
-    repository_name = sys.argv[1]
-    owner_name = sys.argv[2]
-    repository_info = repository_name + "/" + owner_name
-    url = "https://api.github.com/repos/" + repository_info + "/commits"
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
     req = requests.get(url)
-    top = req.json()[:10]
-    for i in top:
-        el = i['sha']
-        author = i['commit']['author']['name']
-        print('{}: {}'.format(el, author))
+    commits = req.json()
+
+    try:
+        for index in range(10):
+            print("{}: {}".format(
+                commits[index].get("sha"),
+                commits[index].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
