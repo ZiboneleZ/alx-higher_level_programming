cde28a0623f5ed8290f6e14e1648fa6ad25bd6d8
Ubuntu <azureuser@first.nrw32hdpzhnuraqadyrvdmrvrh.bx.internal.cloudapp.net>
2024-05-10 12:39:52 +0000

    Another commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""A script that fetches
-   https://alx-intranet.hbtn.io/status
-"""
+"""Script that fetches https://alx-intranet.hbtn.io/status."""
+from urllib.request import Request, urlopen
 
-if __name__ == '__main__':
-    import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')as response:
-        webContent = response.read()
-        print("Body request:")
-        print("\t- type: {}".format(type(webContent)))
-        print("\t- content: {}".format(webContent))
-        print("\t- utf8 content: {}".format(webContent.decode('utf-8')))
+if __name__ == "__main__":
+    req = Request("https://alx-intranet.hbtn.io/status")
+    with urlopen(req) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
