8b6349205fcf1665c24546466f7d3fd4fa976852
Ubuntu <azureuser@first.nrw32hdpzhnuraqadyrvdmrvrh.bx.internal.cloudapp.net>
2024-05-10 15:12:14 +0000

    New one

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
 """
-script that takes in a letter and sends a POST request 
-to http://0.0.0.0:5000/search_user 
-with the letter as a parameter.
+Script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+
+sage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
 """
-
+from sys import argv
 import requests
-import sys
 
-if __name__ == '__main__':
-     q = argv[1] if len(argv) == 2 else ""
-     url = 'http://0.0.0.0:5000/search_user'
-     req = request.post(url, data={'q':q})
-     try:
-         req_dict = req.json()
-         id, name = req_dict.get('id'), req_dict.get('name')
-         if len(req_dict) == 0 or not id or not name:
-             print("No result")
-         else:
-             print("[{}] {}".format(req_dict.get('id'), req_dict.get('name')))
-     except:
-         print("Not a valid")
+
+if __name__ == "__main__":
+    letter = "" if len(argv) == 1 else argv[1]
+    req = requests.post("http://0.0.0.0:5000/search_user", {"q": letter})
+
+    try:
+        response = req.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
