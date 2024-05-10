c68840d6f86abdb78b292ad5634bb997e37285d1
Ubuntu <azureuser@first.nrw32hdpzhnuraqadyrvdmrvrh.bx.internal.cloudapp.net>
2024-05-10 13:25:23 +0000

    Another

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """
-script that takes in a URL, sends a request to the URL and displays 
-the value of the variable X-Request-Id in the response header.
-similar to 1, just using requests on this one
-"""
+Script that takes in a URL, sends a request to the URL and displays the
+value of the variable X-Request-Id in the response header.
 
+Usage: ./5-hbtn_header.py <URL>
+"""
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
-    response = requests.get(sys.argv[1])
-    print(response.headers.get('X-Request-Id'))    
+    url = argv[1]
+    req = requests.get(url)
+
+    print(req.headers.get("X-Request-Id"))  
